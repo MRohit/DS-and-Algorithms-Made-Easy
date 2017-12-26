91f461ec5e0e609998685395142f1f1a026a5819
Rohit <mourya.rohit32@gmail.com>
2017-12-26 23:24:19 +0530

    Solved 37th problem of linked list chapter

--- ch-3 Linked Lists/LLP37.cpp
+++ ch-3 Linked Lists/LLP37.cpp
@@ -48,14 +48,50 @@ void print (sll **head) {
 Method to reverse a linked list
 **/
 sll* reverse (sll *head2) {
-
+  sll *curr = head2, *next = NULL, *temp = NULL;
+  while (curr != NULL) {
+    next = curr->next;
+    curr->next = temp;
+    temp = curr;
+    curr = next;
+  }
+  return temp;
 }
 
 /*
 Method to check if linked list is Palidrome or not
 **/
 void isPalindrome (sll **head) {
+  sll *temp = NULL, *middle = *head, *fast = *head;
+  // 1. Get middle element
+  int cnt = 1;
+  while (fast != NULL) {
+    if(cnt == 1) {
+      fast = fast->next;
+      cnt = 0;
+    } else if (cnt == 0){
+      middle = middle->next;
+      fast = fast->next;
+      cnt = 1;
+    }
+  }
+  cout<<"\nMiddle Node:"<<middle->data<<endl;
+  // 2. Reverse second half of linked list
+  sll *secondHalfReverseList = reverse (middle);
 
+  // 3. Compare first and second half and get the result
+  int flag = 0;
+  sll *first = *head, *second = secondHalfReverseList;
+  while (first->next != middle && second != NULL) {
+    if(first->data != second->data) {
+      flag = 1;
+      cout<<"\nLinked list is not palindrome"<<endl;
+      return;
+    }
+    first = first->next;
+    second = second->next;
+  }
+  cout<<"\nLinked list is palindrome"<<endl;
 }
 
 int main (void) {
@@ -67,11 +103,10 @@ int main (void) {
   insert (&head, 5);
   insert (&head, 4);
   insert (&head, 3);
-  insert (&head, 8);
+  insert (&head, 2);
   insert (&head, 1);
   print (&head);
   isPalindrome (&head);
-  print (&head);
   cout<<endl;
   return 0;
 }
