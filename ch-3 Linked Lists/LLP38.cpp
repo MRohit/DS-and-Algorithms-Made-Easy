434e7fa5a8fd12f29f3718aec67f6f92db8036aa
Rohit <mourya.rohit32@gmail.com>
2018-01-06 18:49:31 +0530

    Solved 38th problem of linked list chapter

---
+++ ch-3 Linked Lists/LLP38.cpp
@@ -0,0 +1,80 @@
+/*
+Problem 38: Reverse linked list in k blocks
+Time Complexity: O(n)
+Space Complexity: O(1)
+**/
+
+#include<iostream>
+#include<stdlib.h>
+using namespace std;
+
+/*
+defining the structure
+**/
+typedef struct sll{
+  int data;
+  struct sll *next;
+}sll;
+
+/*
+Method to insert data into linked list
+**/
+void insert (sll **head, int data) {
+  sll *temp = *head, *newNode = NULL;
+  newNode = (sll*) malloc (sizeof(sll));
+  newNode->data = data;
+  newNode->next = NULL;
+  if (*head == NULL) {
+    *head = newNode;
+    return;
+
+  } else {
+    while (temp->next != NULL)
+      temp = temp->next;
+    newNode->next = temp->next;
+    temp->next = newNode;
+  }
+}
+void print (sll **head) {
+  sll *temp = *head;
+  while (temp != NULL) {
+    cout<<temp->data<<"->";
+    temp = temp->next;
+  }
+}
+
+sll *reverse(sll *head, int k) {
+  sll *next = NULL, *curr = head, *temp = NULL;
+  int i = 0;
+  while (curr != NULL && i < k) {
+    next = curr->next;
+    curr->next = temp;
+    temp = curr;
+    curr = next;
+    i++;
+  }
+  if (next != NULL)
+    head->next = reverse (next,k);
+  return temp;
+}
+int main (void) {
+  sll *head = NULL, *head2;
+
+  insert(&head, 1);
+  insert(&head, 2);
+  insert(&head, 3);
+  insert(&head, 4);
+  insert(&head, 5);
+  insert(&head, 6);
+  insert(&head, 7);
+  insert(&head, 8);
+  insert(&head, 9);
+  insert(&head, 10);
+  insert(&head, 11);
+  insert(&head, 12);
+  print(&head);
+
+  cout<<"\nAfter Reverse:";
+  head2 = reverse (head, 3);
+  print (&head2);
+}
