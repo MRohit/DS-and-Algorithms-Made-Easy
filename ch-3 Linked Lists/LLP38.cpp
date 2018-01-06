82c6c75e980ee7fc812a14237d3a1123f4a14b39
Rohit <mourya.rohit32@gmail.com>
2018-01-06 19:05:25 +0530

    Working on non recursive logic

--- ch-3 Linked Lists/LLP38.cpp
+++ ch-3 Linked Lists/LLP38.cpp
@@ -43,6 +43,10 @@ void print (sll **head) {
   }
 }
 
+/*
+Method to reverse a linked list in given size of pairs
+Time Complexity O(n)
+**/
 sll *reverse(sll *head, int k) {
   sll *next = NULL, *curr = head, *temp = NULL;
   int i = 0;
@@ -57,8 +61,26 @@ sll *reverse(sll *head, int k) {
     head->next = reverse (next,k);
   return temp;
 }
+
+sll *nonRecursiveReverse (sll *head, int k) {
+  sll *next = NULL, *curr = head, *temp = NULL, *temp2 = head;
+  int i = 0;
+  do {
+    i = 0;
+    curr = temp2;
+    while (curr != NULL && i < k) {
+      next = curr->next;
+      curr->next = temp;
+      temp = curr;
+      curr = next;
+    }
+    temp2 = temp;
+  } while (curr != NULL);
+
+  return temp2;
+}
 int main (void) {
-  sll *head = NULL, *head2;
+  sll *head = NULL, *head2, *list2 = NULL;
 
   insert(&head, 1);
   insert(&head, 2);
@@ -77,4 +99,21 @@ int main (void) {
   cout<<"\nAfter Reverse:";
   head2 = reverse (head, 3);
   print (&head2);
+
+  insert(&list2, 11);
+  insert(&list2, 12);
+  insert(&list2, 13);
+  insert(&list2, 14);
+  insert(&list2, 15);
+  insert(&list2, 16);
+  insert(&list2, 17);
+  insert(&list2, 18);
+  insert(&list2, 19);
+  insert(&list2, 20);
+  insert(&list2, 21);
+  insert(&list2, 22);
+  cout<<"\nAfter Reverse (Non Recursive):";
+  head2 = nonRecursiveReverse (list2, 3);
+  print (&head2);
+
 }
