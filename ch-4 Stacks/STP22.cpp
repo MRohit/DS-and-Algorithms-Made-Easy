ff3e100441f91ce038a9f370647a7d0c77764976
Rohit <mourya.rohit32@gmail.com>
2018-01-15 22:32:05 +0530

    Solved 21st and 22nd problem

---
+++ ch-4 Stacks/STP22.cpp
@@ -0,0 +1,19 @@
+/*
+Problem 22: Stock span problem
+Time Complexity: O(n^2)
+Space Complexity: O(1)
+**/
+#include<iostream>
+#include<stdlib.h>
+using namespace std;
+
+void calculateSpan (int arr[], int n) {
+
+}
+int main (void) {
+  int arr[] = {6, 3, 4, 5, 2};
+  int n = arr[0] / sizeof (arr);
+  calculateSpan (arr, n);
+  cout<<endl;
+  return 0;
+}
