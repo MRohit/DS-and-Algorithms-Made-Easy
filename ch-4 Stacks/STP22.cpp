a9afb31df8e95f10f56eec57a9cb1a22c9374463
Rohit <mourya.rohit32@gmail.com>
2018-01-15 22:58:04 +0530

    Solved 22nd problem

--- ch-4 Stacks/STP22.cpp
+++ ch-4 Stacks/STP22.cpp
@@ -8,11 +8,22 @@ Space Complexity: O(1)
 using namespace std;
 
 void calculateSpan (int arr[], int n) {
+  int i, j, span[n];
+  for (int i = 0; i < n; i ++) {
+    j = 1;
+    while (j <= i && arr[i] > arr[i-j]) {
+      j ++;
+    }
+    span[i] = j;
+  }
 
+  for (int i = 0; i < n; i ++) {
+    cout << span[i] << " ";
+  }
 }
 int main (void) {
   int arr[] = {6, 3, 4, 5, 2};
-  int n = arr[0] / sizeof (arr);
+  int n = sizeof (arr) / sizeof (arr[0]);
   calculateSpan (arr, n);
   cout<<endl;
   return 0;
