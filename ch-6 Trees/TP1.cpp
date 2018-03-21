ef56f85347fcc77b79551e5d400961b70a93e3a9
Rohit <mourya.rohit32@gmail.com>
2018-03-22 00:00:37 +0530

    Solved 1st problem of Trees chapter

---
+++ ch-6 Trees/TP1.cpp
@@ -0,0 +1,147 @@
+/*
+  Problem 1: Finding maximum element in binary Tree
+  Time Complexity: O(n)
+  Space Complexity: O(n)
+**/
+
+#include<iostream>
+#include<stdlib.h>
+#include<queue>
+using namespace std;
+
+/*
+defining the structure
+**/
+typedef struct Tree {
+  int data;
+  struct Tree *left, *right;
+}BTree;
+
+typedef struct Queue {
+  int front, rear;
+  int cap,size;
+  BTree **arr;
+}Queue;
+
+Queue *initializeQueue (int size) {
+  Queue *q = (Queue*) malloc (sizeof (Queue));
+  q->front = q->rear = -1;
+  q->cap = size;
+  q->size = 0;
+  q->arr = (BTree**) malloc (sizeof (BTree*) * size);
+  if (!q->arr)
+    return NULL;
+  return q;
+}
+
+int isEmpty (Queue *q) {
+  return q->front == -1;
+}
+int isFull (Queue *q) {
+  return ((q->rear + 1) % q->cap == q->front);
+}
+
+void resizeQueue (Queue *q) {
+  int size = q->cap * 2;
+  q->cap = size;
+
+  q->arr = (BTree**) realloc (q->arr, q->cap);
+  if (!q->arr)
+    return;
+  if (q->front > q->rear) {
+    for (int i = 0; i < q->front; i ++) {
+      q->arr[i + size] = q->arr[i];
+    }
+    q->rear = q->rear + size;
+  }
+}
+void enQueue (Queue *q, BTree *data) {
+  if (isFull (q)) {
+    cout<<"\nQueue is full. Increasing Queue size";
+    resizeQueue (q);
+  }
+    q->rear = (q->rear + 1) % q->cap;
+    q->arr[q->rear] = data;
+    if (q->front == -1)
+      q->front = q->rear;
+    q->size ++;
+
+}
+
+BTree* deQueue (Queue *q) {
+  if (isEmpty (q))
+    return NULL;
+  BTree* data = q->arr[q->front];
+  if (q->front == q->rear)
+    q->front = q->rear = -1;
+  else
+    q->front = (q->front + 1) % q->cap;
+  q->size --;
+  return data;
+}
+
+void deleteQueue (Queue *q) {
+  if (q) {
+    if (q->arr)
+      free (q->arr);
+    free (q);
+  }
+}
+
+BTree* initializeNode () {
+  BTree *root = (BTree*) malloc (sizeof (BTree));
+  if (!root)  return NULL;
+  root->left = root->right = NULL;
+  return root;
+}
+
+void insertNode (BTree **root) {
+  Queue *q = initializeQueue (20);
+  for (int i = 1; i < 10; i ++) {
+    BTree *node = initializeNode ();
+    node->data = i;
+    if (*root == NULL)
+      *root = node;
+    else {
+      BTree *front = q->arr[q->front];
+      if (front->left == NULL)
+        front->left = node;
+      else if (front->right == NULL)
+        front->right = node;
+      if (front->left != NULL && front->right != NULL)
+        deQueue (q);
+    }
+    enQueue (q, node);
+  }
+}
+void preOrder (BTree *root) {
+  if (root) {
+    cout <<root->data<< " ";
+    preOrder (root->left);
+    preOrder (root->right);
+  }
+}
+int getMaximum (BTree *root) {
+  int rootData, left, right, max = -1;
+  if (root) {
+    rootData = root->data;
+    left = getMaximum (root->left);
+    right = getMaximum (root->right);
+    if (left > right)
+      max = left;
+    else
+      max = right;
+    if (rootData > max)
+      max = rootData;
+  }
+  return max;
+}
+int main (void) {
+  BTree *root = NULL;
+  insertNode (&root);
+  cout<<"\nPreorder Traversal:";
+  preOrder (root);
+  cout<<"\nMaximum element in Tree:"<<getMaximum (root);
+  cout<<endl;
+  return 0;
+}
