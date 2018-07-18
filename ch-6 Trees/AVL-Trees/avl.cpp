b163580d4ae6354bf12f9022a6e30a73b1954bda
Rohit <mourya.rohit32@gmail.com>
2018-07-18 23:55:07 +0530

    Learning AVL Trees

--- ch-6 Trees/AVL-Trees/avl.cpp
+++ ch-6 Trees/AVL-Trees/avl.cpp
@@ -17,18 +17,37 @@ defining the structure
 typedef struct Tree {
 
   int data;
+  int height;
   struct Tree *left, *right;
-}BSTree;
+}AVLTree;
 
-BSTree* initializeNode () {
-  BSTree *root = (BSTree*) malloc (sizeof (BSTree));
+AVLTree* initializeNode () {
+  AVLTree *root = (AVLTree*) malloc (sizeof (AVLTree));
   if (!root)  return NULL;
   root->left = root->right = NULL;
   return root;
 }
 
+// Balance Factor = (height of leftsubtree - height of rightSubtree)
+int balanceFactor (AVLTree *root) {
+  int lh, rh;
+  if (root == NULL)
+    return 0;
+  if (root->left == NULL)
+    lh = 0;
+  else
+    lh = root->left->height + 1;
+
+  if (root->right == NULL)
+    rh = 0;
+  else
+    rh = root->right->height + 1;
+  return (lh - rh);
+}
+
+
 int main (void) {
-  
+
   cout<<endl;
   return 0;
 }
